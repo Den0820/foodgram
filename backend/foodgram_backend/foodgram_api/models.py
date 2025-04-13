c111ab3ebee55c909ff720b7e3ea7fbefd8049ae
Denis B <bezdomovdk@mail.ru>
2025-04-13 14:16:15 +0300

    Поехали!

--- backend/foodgram_backend/foodgram_api/models.py
+++ backend/foodgram_backend/foodgram_api/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
